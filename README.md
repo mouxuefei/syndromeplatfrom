469b2809c1697144627dcfa54ef8f3ef6850e7a0
mou <mouxuefei7@dingtalk.com>
2020-05-22 11:18:22 +0800

    feat: 更新点内容

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# weiya
+#### mvp插件
+
+![插件图片](./images/automvp.jpg)
+
--- app/src/main/java/com/huaxi/syndromeplatfrom/mvp/common/view/SplashActivity.kt
+++ app/src/main/java/com/huaxi/syndromeplatfrom/mvp/common/view/SplashActivity.kt
@@ -1,9 +1,12 @@
 package com.huaxi.syndromeplatfrom.view
 
+import com.exmple.corelib.base.LibApplication
+import com.exmple.corelib.exts.myStartActivity
 import com.exmple.corelib.mvp.MBaseMvpActivity
 import com.huaxi.syndromeplatfrom.R
 import com.huaxi.syndromeplatfrom.mvp.common.contract.ISplashContract
 import com.huaxi.syndromeplatfrom.mvp.common.presenter.SplashPresenter
+import com.huaxi.syndromeplatfrom.mvp.common.view.LoginActivity
 import kotlinx.android.synthetic.main.activity_splash.*
 
 /**
@@ -17,10 +20,10 @@ class SplashActivity : MBaseMvpActivity<ISplashContract.View, ISplashContract.Pr
     override fun getContentView()= R.layout.activity_splash
 
     override fun initView() {
-//        LibApplication.appHandler?.postDelayed({
-//            myStartActivity<LoginActivity>()
-//        },1000)
-        aaaa.setOnClickListener {  }
+        LibApplication.appHandler?.postDelayed({
+            myStartActivity<LoginActivity>()
+        },1000)
+//        aaaa.setOnClickListener {  }
     }
 
     override var mPresenter: ISplashContract.Presenter= SplashPresenter()
